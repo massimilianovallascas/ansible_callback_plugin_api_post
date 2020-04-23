c2a8e9b372958883c9cb8aef017a0efd510530b2
Massimiliano Vallascas <massimiliano.vallascas@gmail.com>
2020-04-23 15:18:33 +0100

    Subtree script auto update

--- subtree-init.sh
+++ subtree-init.sh
@@ -2,14 +2,19 @@
 
 ansibleRemoteName="ansible_callback_plugin_api_post"
 ansibleGitRepositoryUrl="git@github.com:massimilianovallascas/ansible_callback_plugin_api_post.git"
+# ansibleGitRepositoryUrl="https://github.com/massimilianovallascas/ansible_callback_plugin_api_post.git"
 ansibleSubtreeFolder="ansible_callback_plugin_api_post"
 
-currentFolder=$(pwd)
+bashScript="subtree-init.sh"
 callbackPluginsFolder="callback_plugins"
 callbackPluginName="api.py"
+currentFolder=$(pwd)
+currentScript="${0}"
 
-callbackPluginSubtreePath=${currentFolder}/${ansibleSubtreeFolder}/${callbackPluginsFolder}/${callbackPluginName}
+bashScriptPath=${currentFolder}/${currentScript}
+bashScriptSubtreePath=${currentFolder}/${ansibleSubtreeFolder}/${bashScript}
 callbackPluginPath=${currentFolder}/${callbackPluginsFolder}/${callbackPluginName}
+callbackPluginSubtreePath=${currentFolder}/${ansibleSubtreeFolder}/${callbackPluginsFolder}/${callbackPluginName}
 
 if [[ ! -z $(git status -s) ]]; then
     echo "Your repository has uncommitted changes, please commit your changes before running this script"
@@ -31,11 +36,16 @@ function copyPlugin {
     cp ${callbackPluginSubtreePath} ${callbackPluginPath}
 }
 
+function copyBashScript {
+    echo "Copying the subtree version of the bash script to ${bashScriptSubtreePath}"
+    cp ${bashScriptSubtreePath} ${bashScriptPath}
+}
+
 if [[ -f "${callbackPluginSubtreePath}" && -f "${callbackPluginPath}" ]]; then
     if ! cmp -s "${callbackPluginSubtreePath}" "${callbackPluginPath}"; then
         echo "Your API Callback Plugin is different from the one in the repository"
-        read -p 'Do you want to update it? [Y/n] : ' update
-        if [[ "${update}" == "y" ]] || [[ ${update} == "Y" ]]; then
+        read -p 'Do you want to update it? [Y/n] : ' updatePlugin
+        if [[ "${updatePlugin}" == "y" ]] || [[ ${updatePlugin} == "Y" ]]; then
             copyPlugin
         else
             echo "Current plugin won't be updated"
@@ -45,6 +55,18 @@ if [[ -f "${callbackPluginSubtreePath}" && -f "${callbackPluginPath}" ]]; then
     fi
 else
     copyPlugin
+fi
+
+if ! cmp -s "${bashScriptSubtreePath}" "${bashScriptPath}"; then
+    echo "Your bash script is different from the one in the repository"
+    read -p 'Do you want to update it? [Y/n] : ' updateScript
+    if [[ "${updateScript}" == "y" ]] || [[ ${updateScript} == "Y" ]]; then
+        copyBashScript
+    else
+        echo "Current bash script won't be updated"
+    fi
+else
+    echo "Your bash script is not different from the one in the repository"
 fi
 
 read -p 'Do you want to delete the subtree folder from the project? (reference in the git config will not be deleted) [Y/n] : ' delete
